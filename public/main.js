3726d599f3c8ece2cee1eb5d5c66b86bf3221878
Jessica Sarkar <jessicalosee@gmail.com>
2022-09-04 18:48:31 -0500

    added likes and delete functionality

--- public/main.js
+++ public/main.js
@@ -1,5 +1,38 @@
 document.querySelector('button').addEventListener('click', getBook)
-const add = document.querySelector('#updateButton')
+const add = document.querySelector('.updateButton')
+const deleteText = document.querySelectorAll('.fa-trash')
+const thumbText = document.querySelectorAll('.fa-thumbs-up')
+
+Array.from(deleteText).forEach((element)=>{
+    element.addEventListener('click', deleteBook)
+})
+
+Array.from(thumbText).forEach((element)=>{
+    element.addEventListener('click', addLike)
+})
+
+
+
+const addBook =  _ => {
+
+    fetch('/booklist', {
+        
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            name: document.querySelector('h3').innerText,
+            author: document.querySelector('h4').innerText,
+            thumbnail: document.querySelector('img').src,
+            
+        })
+    })
+    .then(res => {
+        if (res.ok) return res.json()
+    })
+    .then(response => {
+        window.location.reload(true)
+    })
+}
 
 function getBook(){
     let title = document.querySelector('input').value
@@ -7,11 +40,36 @@ function getBook(){
     fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:${title}`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.items)
-            document.querySelector('h2').innerText = data.items[0].volumeInfo.title
-            document.querySelector('img').src = data.items[0].volumeInfo.imageLinks.thumbnail
+           // console.log(data.items)
+            for(i=0; i<data.items.length; i++){
+                console.log(data.items[i].volumeInfo.title)
+                
+                const newElement = document.createElement('li')
+                newElement.className = "bookResult"
+                document.querySelector(".bookUl").appendChild(newElement)
+
+                const newElementTitle = document.createElement('h3')
+                newElementTitle.innerText = data.items[i].volumeInfo.title
+                document.querySelector(".bookResult").appendChild(newElementTitle)
+
+                const newElementAuthor = document.createElement('h4')
+                newElementAuthor.innerText = data.items[i].volumeInfo.authors
+                document.querySelector(".bookResult").appendChild(newElementAuthor)
+
+                const newElementThumbnail = document.createElement('img')
+                newElementThumbnail.src = data.items[i].volumeInfo.imageLinks.thumbnail
+                document.querySelector(".bookResult").appendChild(newElementThumbnail)
+
+                const button = document.createElement('button')
+                document.querySelector(".bookResult").appendChild(button)
+                button.addEventListener('click', addBook)
+
+                
+            // document.querySelector('h2').innerText = data.items[0].volumeInfo.title
+            /* document.querySelector('img').src = data.items[0].volumeInfo.imageLinks.thumbnail
             document.querySelector('h4').innerText = data.items[0].volumeInfo.authors
-            document.querySelector('h3').innerText = data.items[0].volumeInfo.description
+            document.querySelector('h3').innerText = data.items[0].volumeInfo.description */
+        }
             
     
         })
@@ -21,20 +79,51 @@ function getBook(){
     
 }
 
-add.addEventListener('click', _ => {
-    fetch('/booklist', {
-        method: 'put',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            name: document.querySelector('h2').innerText,
-            author: document.querySelector('h4').innerText,
-            thumbnail: document.querySelector('img').src,
-        })
-    })
-    .then(res => {
-        if (res.ok) return res.json()
-    })
-    .then(response => {
-        window.location.reload(true)
-    })
-})
+async function deleteBook(){
+    const bName = this.parentNode.childNodes[1].innerText
+    const bAuthor = this.parentNode.childNodes[3].innerText
+
+    console.log(bName)
+    console.log(bAuthor)
+    try{
+        const response = await fetch('deleteBook', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'name': bName,
+              'author': bAuthor
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function addLike(){
+    const bName = this.parentNode.childNodes[1].innerText
+    const bAuthor = this.parentNode.childNodes[3].innerText
+     const tLikes = Number(this.parentNode.childNodes[7].innerText)
+    
+    try{
+        const response = await fetch('addOneLike', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'name': bName,
+                'author': bAuthor,
+                'likes': tLikes
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
--- server.js
+++ server.js
@@ -23,32 +23,60 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/',(request, response)=>{
-    db.collection('Cluster0').find().toArray()
+    db.collection('Cluster0').find().sort({likes: -1}).toArray()
     .then(data => {
         response.render('index.ejs', { info: data })
     })
     .catch(error => console.error(error))
 })
 
-app.put('/booklist', (req, res) => {
-    collection.findOneAndUpdate(
-        {name: ''},
+app.post('/booklist', (req, res) => {
+    collection.insertOne(
         {
-            $set: {
+          
                 name: req.body.name,
                 author: req.body.author,
                 thumbnail: req.body.thumbnail,
-            }
+                likes: 0
         },
-       {
-        upsert: true
-       }
     )
     .then(result => {console.log(result)
         res.json('Success')
+        response.redirect('/')
     })
     .catch(error => console.error(error))
 })
+
+app.delete('/deleteBook', (request, response) => {
+
+    db.collection('Cluster0').deleteOne({name: request.body.name})
+    .then(result => {
+        console.log('Book Deleted')
+        response.json('Book Deleted')
+    })
+    .catch(error => console.error(error))
+
+})
+
+app.put('/addOneLike', (request, response) => {
+    db.collection('Cluster0').updateOne({name: request.body.name},{
+        $set: {
+            likes:request.body.likes + 1
+          }
+    },{
+        sort: {_id: -1},
+        upsert: true
+    })
+    .then(result => {
+        console.log('Added One Like')
+        response.json('Like Added')
+        response.redirect('/')
+    })
+    .catch(error => console.error(error))
+
+})
+
+
 
 
 
